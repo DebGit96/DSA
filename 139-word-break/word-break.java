48798978c02cb69448e5ae63f9e4e4d79272d81e
DebGit96 <deb18ban@gmail.com>
2025-08-06 10:42:01 +0530

    Time: 6 ms (61.51%) | Memory: 44.9 MB (16.17%) - LeetSync

---
+++ 139-word-break/word-break.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> dict = new HashSet<>(wordDict);
+        Boolean[] memo= new Boolean[s.length()];
+        return check(0,s, dict,memo);
+    }
+
+    boolean check(int l, String s, Set<String> dict, Boolean[] memo) {
+        if(memo[l]!=null){
+            return memo[l];
+        }
+
+        for (int r = l; r < s.length(); ++r) {
+            String curSubStr = s.substring(l, r + 1);
+            // only change: if we're at the end OR the recursive call succeeds
+            if (dict.contains(curSubStr) && (r == s.length() - 1 || check(r + 1, s, dict,memo))) {
+                memo[l]=true;;
+                return true;
+            }
+        }
+
+        memo[l]=false;
+        return false;
+    }
+}
